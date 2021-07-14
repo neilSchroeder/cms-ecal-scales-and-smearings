bf026cc51f85096c8e7b8314c1d17878829b9903
nschroed <neil.raymond.schroeder@cern.ch>
2021-07-14 19:37:50 +0200

    big changes to the format of pymin.py

--- pymin.py
+++ pymin.py
@@ -45,12 +45,29 @@ def main():
 
     parser.add_argument("-i","--inputFile", required=True, 
                     	help="input file containing paths to data and mc")
+
+    parser.add_argument("--prune", default=False, action='store_true',
+                    	help="prune down the data and mc to keep memory usage low")
+    parser.add_argument("--pruned_file_name", 
+                    	help="basic string for naming the pruned files")
+    parser.add_argument("--pruned_file_dest",
+                    	help="destination for the pruned files, a directory in /eos/home-<initial>/<username>/ is recommended")
+
+    parser.add_argument("--run_divide", default=False, action='store_true',
+                    	help="option to make the run division file for time_stability")
+    parser.add_argument("--minEvents", default=10000,
+                    	help="minimum number of events allowed in a given run bin")
+
+    parser.add_argument("-c","--cats", 
+                    	help="path to file describing categories to use in minimization")
+    parser.add_argument("--time_stability", default=False, action='store_true',
+                    	help="scale data to PDG Z mass, pass the run divide file as your categories")
+
+    #options provided to the minimizer
     parser.add_argument("-s","--scales",
                     	help="path to scales file to apply to data")
     parser.add_argument("--smearings", default=None,
                         help="path to smearings file to apply to MC")
-    parser.add_argument("-c","--cats", 
-                    	help="path to file describing categories to use in minimization")
     parser.add_argument("-w", "--weights", default='',
                         help="tsv containing rapidity x ptz weights, if empty, they will be derived. It is recommended that these be derived just after deriving time stability (step1) corrections.")
     parser.add_argument("-o","--output", default='',
@@ -71,72 +88,56 @@ def main():
                     	help="Step size for scan start")
     parser.add_argument("--min_step_size", default=None, 
                         help="Min step size for scipy.optimize.minimize function. This is an advanced option, please use with care.")
-    parser.add_argument("--prune", default=False, action='store_true',
-                    	help="prune down the data and mc to keep memory usage low")
-    parser.add_argument("--pruned_file_name", 
-                    	help="basic string for naming the pruned files")
-    parser.add_argument("--pruned_file_dest",
-                    	help="destination for the pruned files, a directory in /eos/home-<initial>/<username>/ is recommended")
-    parser.add_argument("--run_divide", default=False, action='store_true',
-                    	help="option to make the run division file for time_stability")
-    parser.add_argument("--minEvents", default=10000,
-                    	help="minimum number of events allowed in a given run bin")
-    parser.add_argument("--time_stability", default=False, action='store_true',
-                    	help="scale data to PDG Z mass, pass the run divide file as your categories")
     parser.add_argument("--closure", default=False, action='store_true',
                     	help="derive the closure of the scales for a given step")
+    parser.add_argument("--no_auto_bin", default=False, action='store_true',
+                    	help="Turns off the auto binning feature (using Freedman-Diaconis method)")
+    parser.add_argument("--bin_size", default=0.25, type=float,
+                    	help="Size of bins for binned NLL evaluation")
+    parser.add_argument("--fix_scales", default=False, action='store_true',
+                        help="[ADVANCED] flag to keep scales fixed at 1. and only derive smearings")
+
+    #additional use options
     parser.add_argument("--rewrite", default=False, action='store_true',
                     	help="only writes the scales file associated with this step")
+    parser.add_argument("--combine_files", default=False, action='store_true',
+                    	help="[ADVANCED] combines two specified files, using the --only_step and --step options")
+    parser.add_argument("--only_step", default='', type=str,
+                    	help="[ADVANCED] only step file, to be used with the --combine_files and --step options")
+
+    #plotting options
     parser.add_argument("--plot", default=False, action='store_true',
                     	help="Plot the invariant mass distributions for data and mc in the provided categories")
     parser.add_argument("--plot_dir", default='./',
                     	help="directory to write the plots")
+
+    #advanced diagnostic options
     parser.add_argument("--test_method_accuracy", default=False, action='store_true',
                     	help="Treat MC as data, inject known scales into mc cats, see how well method recovers known scale injection")
     parser.add_argument("--scan_nll", default=False, action='store_true',
                     	help="Scan the NLL phase space for a given set of categories. A set of scales in the 'onlystepX' format should be provided as the scan center")
     parser.add_argument("--scan_scales", default='', 
                     	help="Scales defining the center of the scan [this is highly recommended when scanning the NLL phase space]")
-    parser.add_argument("--no_auto_bin", default=False, action='store_true',
-                    	help="Turns off the auto binning feature (using Freedman-Diaconis method)")
-    parser.add_argument("--bin_size", default=0.25, type=float,
-                    	help="Size of bins for binned NLL evaluation")
     parser.add_argument("--condor", default=False, action='store_true',
                         help="Submit this job to condor")
     parser.add_argument("--queue", default='tomorrow',
                         help="flavour of submitted job")
     parser.add_argument("--from_condor", default=False, action='store_true',
                         help="[NOT FOR USER] flag added by condor_handler to indicate this has been submitted from condor")
-    parser.add_argument("--fix_scales", default=False, action='store_true',
-                        help="[ADVANCED] flag to keep scales fixed at 1. and only derive smearings")
-    parser.add_argument("--combine_files", default=False, action='store_true',
-                    	help="[ADVANCED] combines two specified files, using the --only_step and --step options")
-    parser.add_argument("--only_step", default='', type=str,
-                    	help="[ADVANCED] only step file, to be used with the --combine_files and --step options")
 
     args = parser.parse_args()
     print("[INFO] welcome to SS_PyMin")
     print("[INFO] you have run the following command:")
-    cmd = ''
-    for arg in sys.argv:
-        if ' ' in arg:
-            cmd += '"{}"  '.format(arg)
-        else:
-            cmd +="{}  ".format(arg)
-    print(cmd) 
+
+    print(helper_pymin.get_run_string(sys.argv))
 
     if args.closure and args.smearings is None:
         print("[ERROR] you have submitted a closure test without a smearings file.")
         print("[ERROR] please resubmit this job with a smearings file.")
         return
 
-    step = -1
-    if args.cats is not None and not args.time_stability: 
-        step = int(args.cats[args.cats.find("step")+4])
+    step = helper_pymin.get_step(args)
 
-###############################################################################
-
-###############################################################################
     #submit this job to condor
     if args.condor and not args.from_condor:
         #remove the condor/queue information
@@ -146,90 +147,40 @@ def main():
         condor_handler.manage(cmd, args.output, args.queue) 
         return
 
-###############################################################################
-
-###############################################################################
     #if you need to rewrite a scales file after making changes by hand
-    # you can use this option to do so.
+    # you can use these options to do so.
     if args.rewrite:
-        scales_out = os.getcwd()+"/datFiles/step"+str(step)+"_"+args.output+"_scales.dat"
-        if args.scales != '':
-            scales_out = os.path.dirname(args.scales)+"/step"+str(step)+"_"+args.output+"_scales.dat"
-        if args.from_condor:
-            scales_out = os.getcwd()+"/condor/"+args.output+"/step"+str(step)+"_"+args.output+"_scales.dat"
-        if args.closure: scales_out = scales_out.replace("step"+str(step), "step"+str(step)+"closure",1)
-        else: scales_out = scales_out.replace("step"+str(step-1),"step"+str(step),1)
-        new_scales = scales_out.replace("step", "onlystep")
-        new_smears = os.path.dirname(scales_out)+"/"+os.path.basename(scales_out).replace("scales", "smearings")
-        if not args.closure: write_files.rewrite_smearings(args.cats, new_smears)
-        write_files.combine( new_scales, args.scales, scales_out )
+        helper_pymin.rewrite(args)
         return
 
     if args.combine_files:
-        scales_out = os.getcwd()+"/datFiles/step"+str(step)+"_"+args.output+"_scales.dat"
-        if args.scales != '':
-            scales_out = os.path.dirname(args.scales)+"/step"+str(step)+"_"+args.output+"_scales.dat"
-        if args.from_condor:
-            scales_out = os.getcwd()+"/condor/"+args.output+"/step"+str(step)+"_"+args.output+"_scales.dat"
-        if args.closure: scales_out = scales_out.replace("step"+str(step), "step"+str(step)+"closure",1)
-        else: scales_out = scales_out.replace("step"+str(step-1),"step"+str(step),1)
-        write_files.combine( args.only_step, args.scales, scales_out )
+        helper_pymin.combine_files(args)
         return
 
-###############################################################################
-
-###############################################################################
     #prune files to manage running memory usage
     if args.prune:
         pruner.prune(args.inputFile, args.pruned_file_name, args.pruned_file_dest)
         return
 
-###############################################################################
-
-###############################################################################
-
     #load data/mc
-    data = pd.DataFrame()
-    mc = pd.DataFrame()
-
-    root_files = open(args.inputFile, 'r').readlines()
-    root_files = [x.strip() for x in root_files]
-
-    #import data and mc to dataframes
-    print("[INFO] importing data and mc to dataframes (this might take a bit) ...")
-    if root_files[0].find("data") != -1:
-        data = pd.read_csv(root_files[0], sep='\t')
-        mc = pd.read_csv(root_files[1], sep='\t')
-    elif root_files[1].find("data") != -1:
-        data = pd.read_csv(root_files[1], sep='\t')
-        mc = pd.read_csv(root_files[0], sep='\t')
-    else:
-        print("[ERROR] could not find a data file to open")
-        return
-    if args.test_method_accuracy:
-        data = mc.copy()
-
-    data['etaEle[0]'] = np.absolute(data['etaEle[0]'].values)
-    data['etaEle[1]'] = np.absolute(data['etaEle[1]'].values)
-    mc['etaEle[0]'] = np.absolute(mc['etaEle[0]'].values)
-    mc['etaEle[1]'] = np.absolute(mc['etaEle[1]'].values)
+    data, mc = helper_pymin.load_dataframes(args.inputFile)
     
+    if data is None or mc is None:
+        print("[ERROR] data or MC is empty")
+        return
 
-###############################################################################
     #derive time bins (by run) in which to stabilize data
     if args.run_divide:
         outFile = "datFiles/run_divide_"+args.output+".dat"
         write_files.write_runs(divide_by_run.divide(data, args.minEvents), outFile)
         return
 
-###############################################################################
     #derive the scales for the time stability
     if args.time_stability:
         outFile = "datFiles/step1_"+args.output+"_scales.dat"
         write_files.write_time_stability(time_stability.derive(data, args.cats), args.cats, outFile)
         return
 
-###############################################################################
     #scale the data
     if args.scales:
         print("[INFO] applying {} to the data".format(args.scales))
@@ -271,29 +222,10 @@ def main():
         print("[ERROR] Review code, minimization did not succeed") 
         return
 
-###############################################################################
-    #write the onlystepX file and smearings file
+    #write the results
+    helper_pymin.write_results(args, scales_smears, unc)
 
-    scales_out = os.path.dirname(args.scales)+"/step"+str(step)+"_"+args.output+"_scales.dat"
-    if args.scales != '':
-        scales_out = os.path.dirname(args.scales)+"/step"+str(step)+"_"+args.output+"_scales.dat"
-    if args.from_condor:
-        scales_out = os.getcwd()+"/condor/"+args.output+"/step"+str(step)+"_"+args.output+"_scales.dat"
-    if args.closure: scales_out = scales_out.replace("step"+str(step), "step"+str(step)+"closure",1)
-    else: scales_out = scales_out.replace("step"+str(step-1),"step"+str(step),1)
+    return
 
-    new_scales = os.path.dirname(scales_out)+"/"+os.path.basename(scales_out).replace("step", "onlystep")
-    new_smears = os.path.dirname(scales_out)+"/"+os.path.basename(scales_out).replace("scales", "smearings")
-
-    scales_smears = [(scales_smears[i], unc[i]) for i,x in enumerate(unc)]
-    write_files.write_scales(scales_smears[:num_scales], cats, new_scales)
-    if not args.closure: write_files.write_smearings(scales_smears, cats, new_smears)
-###############################################################################
-
-###############################################################################
-    #make scales file here
-    print("[INFO] creating new scales file: {}".format(scales_out))
-    write_files.combine( new_scales, args.scales, scales_out )
-###############################################################################
-
-main()
+if __name__ == "__main__":
+    main()
