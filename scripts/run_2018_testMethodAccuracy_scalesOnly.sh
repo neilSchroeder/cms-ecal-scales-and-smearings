ec279561e56412d9db364088063d7301934c6c56
nschroed <neil.raymond.schroeder@cern.ch>
2021-08-04 21:23:10 +0200

    added another script

---
+++ scripts/run_2018_testMethodAccuracy_scalesOnly.sh
@@ -0,0 +1,13 @@
+./pymin.py \
+    -i config/ul2018.dat \
+    -c config/cats_step2.tsv \
+    -o ul18_testMethodAccuracy_scaleOnly_v1 \
+    --no-reweight \
+    --scan-min=0.99 \
+    --scan-max=1.01 \
+    --scan-step=0.0005 \
+    --min-step=0.0001 \
+    --test-method-accuracy \
+    --closure \
+    --condor \
+    --queue='nextweek'
