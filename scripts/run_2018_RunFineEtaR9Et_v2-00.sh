68701c96366385bda442f13fa485723825f8eb23
nschroed <neil.raymond.schroeder@cern.ch>
2021-09-20 18:10:38 +0200

    added script for UL18 Et scales

---
+++ scripts/run_2018_RunFineEtaR9Et_v2-00.sh
@@ -0,0 +1,16 @@
+./pymin.py \
+    -i config/ul2018.dat \
+    -c config/cats_step4_et.tsv \
+    -s ~/public/scales/Run2018/step2closure_ul18_2021-09-08_stochastic_v3-05_scales.dat \
+    --smearings="/afs/cern.ch/user/n/nschroed/public/scales/Run2018/Run2018_2021-09-03_RunFineEtaR9Et_stochastic_v3-05_smearings.dat" \
+    -w datFiles/ptz_x_rapidity_weights_ul18_2021-07-15_testOverhaul.tsv \
+    -o ul18_2021-09-17_stochastic_v2-00 \
+    --closure \
+    --min-step=0.0001 \
+    --scan-min=0.995 \
+    --scan-max=1.005 \
+    --scan-step=0.001 \
+    --hist-min=82 \
+    --hist-max=98 \
+    --condor \
+    --queue="nextweek"
