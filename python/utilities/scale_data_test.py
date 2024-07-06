23bcba3843c7a832dcb9817e771f559560947071
Neil Schroeder <neil.raymond.schroeder@cern.ch>
2024-07-05 19:24:49 -0500

    MINOR: trying to fix issues with gain

--- python/utilities/scale_data_test.py
+++ python/utilities/scale_data_test.py
@@ -21,10 +21,15 @@ def prepare_scales_lookup(scales_df):
     scales_df = scales_df.sort_values([dc.i_run_min, dc.i_eta_min, dc.i_r9_min, dc.i_et_min])
     
     # Create bin edges
-    run_edges = np.unique(np.concatenate([scales_df[dc.i_run_min], scales_df[dc.i_run_max]]))
-    eta_edges = np.unique(np.concatenate([scales_df[dc.i_eta_min], scales_df[dc.i_eta_max]]))
-    r9_edges = np.unique(np.concatenate([scales_df[dc.i_r9_min], scales_df[dc.i_r9_max]]))
-    et_edges = np.unique(np.concatenate([scales_df[dc.i_et_min], scales_df[dc.i_et_max]])) if all(scales_df[dc.i_gain] == 0) else np.array([0, 1, 6, 12])
+    run_edges = np.unique(scales_df[[dc.i_run_min, dc.i_run_max]].values)
+    eta_edges = np.unique(scales_df[[dc.i_eta_min, dc.i_eta_max]].values)
+    r9_edges = np.unique(scales_df[[dc.i_r9_min, dc.i_r9_max]].values)
+    et_edges = np.unique(scales_df[[dc.i_et_min, dc.i_et_max]].values) if all(scales_df[dc.i_gain] == 0) else np.array([0.5, 5.5, 6.5, 12.5])
+
+    print(run_edges)
+    print(eta_edges)
+    print(r9_edges)
+    print(et_edges)
 
     # Create lookup array
     lookup_scales = np.full((len(run_edges)-1, len(eta_edges)-1, len(r9_edges)-1, len(et_edges)-1), np.nan)
