0cbb5089f0d32d301d7223442d4c4e3aebf48a4d
neilSchroeder <schr1077@umn.edu>
2025-03-06 16:13:04 -0700

    fixing num args passed to enhanced target function wrapper

--- src/tools/target_function.py
+++ src/tools/target_function.py
@@ -710,7 +710,7 @@ def adaptive_scan_nll(x, **options):
     
     # Create the loss function wrapper
     loss_function, reset_loss_initial_guess, _ = enhanced_target_function_wrapper(
-        guess, __ZCATS__, options.get('num_scales', -1), options.get('num_smears', -1), **options
+        guess, __ZCATS__, **options
     )
     
     # Define a helper function for parameter optimization that works for both scales and smearings
